44d3935646b2810f53472b127df6f036c2c3d58f
mmvijayaramkumar <mmvijayaramkumar@gmail.com>
2019-01-03 14:44:12 +0530

    Add initial Jenkinsfile

--- backend/test/test.js
+++ backend/test/test.js
@@ -20,3 +20,13 @@ describe('GET /tasklist', function() {
         })
     })
 });
+
+describe('GET /endtask', function() {
+  it('should return status code 404', function(done) {
+    var base_url = "http://localhost:3000/endtask"
+      request.get(base_url, function(err,response,body){
+          expect(response.statusCode).toBe(404);
+          done();
+      })
+  })
+});
